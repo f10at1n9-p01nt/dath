014bb9507ea626706ff4cd70659682ef03af5e1e
Kevin Yaiko <keyaiko@icsbudapest.org>
2020-04-01 11:33:28 +0200

    Add checkbox to todo item

--- js/todoApp.js
+++ js/todoApp.js
@@ -1,12 +1,20 @@
 const submitted = document.querySelector('form');
-const newTodoInput = document.querySelector('input[name="newtodo"]');
 const ul = document.querySelector('ul');
-let newLi = document.createElement('li');
+
+function addItem() {
+	const newTodoInput = document.querySelector('input[name="newtodo"]');
+	const newLi = document.createElement('li');
+	const newCheckbox = document.createElement('input');
+	newCheckbox.type = 'checkbox';
+	newCheckbox.value = 1;
+
+	newLi.innerText = newTodoInput.value;
+	ul.append(newLi);
+	newLi.append(newCheckbox);
+	newTodoInput.value = '';
+}
 
 submitted.addEventListener('submit', function(event) {
 	event.preventDefault();
-	const newLi = document.createElement('li');
-	newLi.innerText = newTodoInput.value;
-	ul.append(newLi);
-	newTodoInput.value = '';
+	addItem();
 });
